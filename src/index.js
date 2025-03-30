a2f5dc7f0618fb4fc8e6246b3434baaef687cf2c
mvsantss <mayconsan01@outlook.com>
2025-03-30 06:31:52 -0300

    Atualiza código com ajustes nos serviços e deploy

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import { readdirSync } from 'fs';
 import config from '../config/config.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
